715d48a1c8e580018c405f436d2e32421e1cc40b
Abdul Moiz <162945971+Abdulm01z@users.noreply.github.com>
2024-03-25 07:31:03 +0500

    Add files via upload

---
+++ 4 (Famous Quote)/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let qoute = "Let the future tell the truth and evaluate each one according to his work and accomplishments.";
+let femousPerson = "Nikola Tesla";
+console.log(`${femousPerson} once said, ${qoute}`);
---
+++ 4 (Famous Quote)/main.ts
@@ -0,0 +1,3 @@
+let qoute: string = "Let the future tell the truth and evaluate each one according to his work and accomplishments.";
+let femousPerson: string = "Nikola Tesla";
+console.log(`${femousPerson} once said, ${qoute}`);
