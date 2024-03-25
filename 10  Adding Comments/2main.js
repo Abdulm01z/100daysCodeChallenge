1dac1a99a51bb74cf87ff6c479b124aa407f4a00
Abdul Moiz <162945971+Abdulm01z@users.noreply.github.com>
2024-03-25 07:44:20 +0500

    Add files via upload

---
+++ 10  Adding Comments/2main.js
@@ -0,0 +1,5 @@
+"use strict";
+//exercise 10
+// My name is Abdul Moiz 
+// Dated: 3/19/24
+console.log(5 * 2);
---
+++ 10  Adding Comments/main.ts
@@ -0,0 +1,4 @@
+//exercise 10
+// My name is Abdul Moiz 
+// Dated: 3/19/24
+console.log('Hello world simple program');
